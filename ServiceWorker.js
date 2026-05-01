a9bf3522cff76a9e57852a844cf773e79edd4f60
CavemanGaming <126614429+Veryhappyfish1@users.noreply.github.com>
2026-04-30 23:54:22 -0500

    game lol

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "RLP-MANIC Episode 3-2";
+const contentToCache = [
+    "Build/manic 3 html build.loader.js",
+    "Build/manic 3 html build.framework.js",
+    "Build/manic 3 html build.data",
+    "Build/manic 3 html build.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
